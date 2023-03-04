e04418a5e5d3511ac5f26167b3c3a9ffba88fce0
Marvin Bornstein <marvinbornstein@gmail.com>
2023-03-04 12:27:22 +0000

    edit event data

--- 30th/js/loadEventData.js
+++ 30th/js/loadEventData.js
@@ -1,8 +1,7 @@
 var partyData = {
-  "headline": "Thirsty Thirty",
-  "subheadline": "HOSTED BY CARL & MARVIN",
-  "shortDescription": "Taking our BIRTHDAY PARTY to the next level - exactly, it's a ROOFTOP HEATING. " +
-    "<br />So please join us to celebrate the our round birthday with music and drinks in Berlin.",
+  "headline": "The Roaring Thirties",
+  "subheadline": "a soirée by Carl & Marvin",
+  "shortDescription": "Saturn has orbited around the sun as we embark to the next decade. Join us to celebrate and for the night with music, drinks, and good vibes in Berlin.",
   "date": "Saturday, 16. September 2023",
   "time": "16:00 - 03:00 CET",
   "location": "Location will be announced later this year.",
